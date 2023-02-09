a9e168ad6c0edb63def9ff4445c4069e501bb360
nobodycase <3417015553@qq.com>
2023-02-09 16:49:44 +0800

    feat(page): 保存历史记录、把状态从Board提升至Game

--- src/index.js
+++ src/index.js
@@ -12,58 +12,19 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  // 当前 state 没有保存在单个的 Square 组件中，而是保存在了 Board 组件中。
-  // 每当 Board 的 state 发生变化的时候，这些 Square 组件都会重新渲染一次。
-  // 把所有 Square 的 state 保存在 Board 组件中可以让我们在将来判断出游戏的胜者。
-
-  // 因为 Square 组件不再持有 state，因此每次它们被点击的时候，Square 组件就会从 Board 组件中接收值，并且通知 Board 组件。
-  // 在 React 术语中，我们把目前的 Square 组件称做“受控组件”。在这种情况下，Board 组件完全控制了 Square 组件。
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true, // x 为先手棋
-    };
-  }
-
-  handleClick(i) {
-    // 这里 slice 什么用？ 用 .slice() 方法创建了 squares 数组的一个副本，而不是直接在现有的数组上进行修改
-    const squares = this.state.squares.slice();
-
-    // 当有玩家胜出时，或者某个 Square 已经被填充时，该函数不做任何处理直接返回
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
-    squares[i] = this.state.xIsNext ? "X" : "O"; // 棋子落下
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext, // 棋子落下，下一步是否轮到x就改变
-    });
-  }
-
   renderSquare(i) {
     return (
+      // 让 Board 组件从 Game 组件中接收 squares 和 onClick 这两个 props
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.squares(i)}
       />
-    ); // Square 的 this.props = {value: i}
+    );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "x" : "o");
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -85,14 +46,56 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    // 初始化 state
+    this.state = {
+      history: [{ squares: Array(9).fill(null) }],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    // 这里 slice 什么用？ 用 .slice() 方法创建了 squares 数组的一个副本，而不是直接在现有的数组上进行修改
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    // 当有玩家胜出时，或者某个 Square 已经被填充时，该函数不做任何处理直接返回
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? "X" : "O"; // 棋子落下
+    this.setState({
+      history: history.concat([{ squares: squares }]),
+      xIsNext: !this.state.xIsNext, // 棋子落下，下一步是否轮到x就改变
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
     return (
+      // 使用最新一次历史记录来确定并展示游戏的状态
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
