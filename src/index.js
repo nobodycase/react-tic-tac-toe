71a2a304a90bd3c88da11541600bf5074ee8099a
nobodycase <3417015553@qq.com>
2023-02-09 15:22:07 +0800

    refactor(page): state从保存在Square改为保存在Board

--- src/index.js
+++ src/index.js
@@ -3,25 +3,45 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 class Square extends React.Component {
-  // 构造函数，props 为接收的参数
-  constructor(props) {
-    super(props); // 定义子类的构造函数时，都需要调用 super 方法
-    this.state = { value: null }; // 给 Square 类定义一个属性 state
-  }
-
   render() {
     return (
       // this.setState 更新 state 的数据
-      <button className="square" onClick={() => this.setState({ value: "x" })}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  // 当前 state 没有保存在单个的 Square 组件中，而是保存在了 Board 组件中。
+  // 每当 Board 的 state 发生变化的时候，这些 Square 组件都会重新渲染一次。
+  // 把所有 Square 的 state 保存在 Board 组件中可以让我们在将来判断出游戏的胜者。
+
+  // 因为 Square 组件不再持有 state，因此每次它们被点击的时候，Square 组件就会从 Board 组件中接收值，并且通知 Board 组件。
+  // 在 React 术语中，我们把目前的 Square 组件称做“受控组件”。在这种情况下，Board 组件完全控制了 Square 组件。
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    // 这里 slice 什么用？ 用 .slice() 方法创建了 squares 数组的一个副本，而不是直接在现有的数组上进行修改
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />; // Square 的 this.props = {value: i}
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    ); // Square 的 this.props = {value: i}
   }
 
   render() {
