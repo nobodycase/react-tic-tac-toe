3301164bc2d5ea6e7b426f2a3c1a9312946f1b6a
nobodycase <3417015553@qq.com>
2023-02-09 16:14:38 +0800

    feat(page): 判断胜出者

--- src/index.js
+++ src/index.js
@@ -30,6 +30,12 @@ class Board extends React.Component {
   handleClick(i) {
     // 这里 slice 什么用？ 用 .slice() 方法创建了 squares 数组的一个副本，而不是直接在现有的数组上进行修改
     const squares = this.state.squares.slice();
+
+    // 当有玩家胜出时，或者某个 Square 已经被填充时，该函数不做任何处理直接返回
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? "X" : "O"; // 棋子落下
     this.setState({
       squares: squares,
@@ -47,7 +53,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "x" : "o");
+    }
 
     return (
       <div>
@@ -92,3 +104,24 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
+
+// 传入长度为 9 的数组，此函数将判断出获胜者，并根据情况返回 “X”，“O” 或 “null”。
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
